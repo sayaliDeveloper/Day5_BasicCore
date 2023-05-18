914d9f6dabcc42f3f5495bb6983e8bd73227ad61
Uptech <uptech@trichemie.com>
2023-05-18 21:27:43 +0530

    Added:Java Program for Flip Coin and print percentage of Heads and Tails

---
+++ src/com/bridgelabz/corePrograms/CoreProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.corePrograms;
+
+import java.util.Scanner;
+
+public class CoreProgram {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
