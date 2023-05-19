537aeeccc8e5526175804b5558dc44ffef8f9601
Uptech <uptech@trichemie.com>
2023-05-19 08:00:00 +0530

    Added:Java Program to Swap Two Numbers

--- src/com/bridgelabz/corePrograms/CoreProgram.java
+++ src/com/bridgelabz/corePrograms/CoreProgram.java
@@ -11,6 +11,7 @@ public class CoreProgram {
         harmonicNumber();
         primeFactor();
         quotientAndRemainder();
+        swapNumber();
     }
 
     public static void flipCoin() {
@@ -133,6 +134,30 @@ public class CoreProgram {
         System.out.println("Remainder: " + remainder);
 
     }
+    public static void swapNumber(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number one to Swap: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Number Two to Swap: ");
+        int num2 = sc.nextInt();
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        }
+
+
+
+
+
+
+
 
 }
 
