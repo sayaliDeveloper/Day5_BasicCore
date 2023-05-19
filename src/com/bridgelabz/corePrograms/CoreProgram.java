92de0b678de14f70f57d90a4ff225ddf8fc13176
Uptech <uptech@trichemie.com>
2023-05-19 07:47:45 +0530

    Added: Java Program to Compute Quotient and Remainder

--- src/com/bridgelabz/corePrograms/CoreProgram.java
+++ src/com/bridgelabz/corePrograms/CoreProgram.java
@@ -4,62 +4,61 @@ import java.util.Scanner;
 
 public class CoreProgram {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         flipCoin();
         leapYear();
-       // powerOfTwo();
+        // powerOfTwo();
         harmonicNumber();
         primeFactor();
+        quotientAndRemainder();
     }
 
     public static void flipCoin() {
 
-                System.out.println("Enter how many times you want to flip the coin :");
-                Scanner sc=new Scanner(System.in);
-                int count=sc.nextInt();
-                int headFlip=0;
-                int tailFlip=0;
-                if (count>0) {
-                    for (int i=1; i<=count;i++) {
-                        double flip=Math.floor(Math.random()*10)%2;
-                        if (flip<0.5) {
-                            headFlip++;
-                        }
-                        else {
-                            tailFlip++;
-                        }
-                    }
+        System.out.println("Enter how many times you want to flip the coin :");
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int headFlip = 0;
+        int tailFlip = 0;
+        if (count > 0) {
+            for (int i = 1; i <= count; i++) {
+                double flip = Math.floor(Math.random() * 10) % 2;
+                if (flip < 0.5) {
+                    headFlip++;
+                } else {
+                    tailFlip++;
                 }
-                else {
-                    System.out.println("Entered wrong value Please Enter Positive Integer");
-                }
-                double headPercent=(headFlip*100)/count;
-                double tailPercent=(tailFlip*100)/count;
-                System.out.println("Head flip percentage is : "+headPercent);
-                System.out.println("Tail flip percentage is : "+tailPercent);
+            }
+        } else {
+            System.out.println("Entered wrong value Please Enter Positive Integer");
+        }
+        double headPercent = (headFlip * 100) / count;
+        double tailPercent = (tailFlip * 100) / count;
+        System.out.println("Head flip percentage is : " + headPercent);
+        System.out.println("Tail flip percentage is : " + tailPercent);
 
 
     }
-    public static void leapYear(){
+
+    public static void leapYear() {
         int year;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter  Any  4 Digit Number :");
-        year=sc.nextInt();
-        if(year > 999 && year < 10000) {
-            if(year%400==0)
-                System.out.println(year+" is a leap year");
-            else  if(year%100!=0) {
-                if(year%4==0)
-                    System.out.println(year+" is a leap year");
+        year = sc.nextInt();
+        if (year > 999 && year < 10000) {
+            if (year % 400 == 0)
+                System.out.println(year + " is a leap year");
+            else if (year % 100 != 0) {
+                if (year % 4 == 0)
+                    System.out.println(year + " is a leap year");
                 else
-                    System.out.println(year+" is not a leap year");
-            }
-            else
-                System.out.println(year+" is not a leap year");
-        }
-        else
+                    System.out.println(year + " is not a leap year");
+            } else
+                System.out.println(year + " is not a leap year");
+        } else
             System.out.println("Year zero does not exist ");
     }
+
     /*public static void powerOfTwo(){
                 if (args.length < 1) {
                     System.out.println("Enter value for N as a command-line argument.");
@@ -80,40 +79,60 @@ public class CoreProgram {
                         }
                     }
                 }*/
-                     public static void harmonicNumber() {
-                         Scanner sc=new Scanner(System.in);
-                         System.out.print("Enter Value of Number:");
-                         int n = sc.nextInt();
+    public static void harmonicNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of Number:");
+        int n = sc.nextInt();
 
-                         if (n <= 0) {
-                             System.out.println(" N should be a positive integer.");
-                         } else {
-                             double harmonic = 0.0;
+        if (n <= 0) {
+            System.out.println(" N should be a positive integer.");
+        } else {
+            double harmonic = 0.0;
 
-                             for (int i = 1; i <= n; i++) {
-                                 harmonic += 1.0 / i;
-                             }
+            for (int i = 1; i <= n; i++) {
+                harmonic += 1.0 / i;
+            }
 
-                             System.out.println("The " + n + "th Harmonic Value is: " + harmonic);
-                         }
+            System.out.println("The " + n + "th Harmonic Value is: " + harmonic);
+        }
 
-
-                     }
-                     public static void primeFactor(){
-                         Scanner sc = new Scanner(System.in);
-                         System.out.print("Enter a number: ");
-                         int num = sc.nextInt();
-                         System.out.print("Prime factors of " + num + ": ");
-                         for (int i = 2; i * i <= num; i++) {
-                             while (num % i == 0) {
-                                 System.out.print(i + " ");
-                                 num  = num/i;
-                             }
-                         }
-                         if (num > 1) {
-                             System.out.print(num);
-                         }
 
     }
+
+    public static void primeFactor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.print("Prime factors of " + num + ": ");
+        for (int i = 2; i * i <= num; i++) {
+            while (num % i == 0) {
+                System.out.print(i + " ");
+                num = num / i;
+            }
+        }
+        if (num > 1) {
+            System.out.print(num);
+        }
+
+    }
+
+    public static void quotientAndRemainder() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter dividend: ");
+        int dividend = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+    }
+
 }
 
